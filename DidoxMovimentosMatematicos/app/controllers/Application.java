45cbb7608f7d67eba37dc3b5287547123c3144fb
Didox <didox_59@yahoo.com.br>
2013-09-05 22:40:10 -0300

    criando while e rota para adicionar

--- DidoxMovimentosMatematicos/app/controllers/Application.java
+++ DidoxMovimentosMatematicos/app/controllers/Application.java
@@ -2,16 +2,31 @@ package controllers;
 
 import org.joda.time.DateTime;
 
+import com.avaje.ebean.common.BeanList;
+
 import models.Task;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.novo;
 
 public class Application extends Controller {
   
     public static Result index() {
-    	new Task("Didox", true, new DateTime()).save();
-        return ok(index.render("Movimentos Matematicos " + Task.find.all().get(0).name));
+    	BeanList<Task> tasks = (BeanList<Task>) Task.find.all();
+        return ok(index.render(tasks));
+    }
+    
+    public static Result novo(){
+    	return ok(novo.render());
+    }
+    
+    public static Result create(String nome){
+    	if(nome != ""){
+			new Task(nome, true, new DateTime()).save();
+	    	return ok("Criado com sucesso");
+    	}
+    	return badRequest("Erro ao criar");
     }
   
 }
